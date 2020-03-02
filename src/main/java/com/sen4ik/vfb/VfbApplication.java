ad82aab0ed67f4fe948ed39c27cb577742002c30
Artur Sentsov <sen4ik@gmail.com>
2020-03-01 17:51:41 -0800

    initial commit

---
+++ src/main/java/com/sen4ik/vfb/VfbApplication.java
@@ -0,0 +1,13 @@
+package com.sen4ik.vfb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VfbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VfbApplication.class, args);
+	}
+
+}
