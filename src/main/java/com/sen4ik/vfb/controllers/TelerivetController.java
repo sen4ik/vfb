ec9710e2c324259a57b9faf62ab41af3a082af03
Artur Sentsov <sen4ik@gmail.com>
2020-03-18 19:49:02 -0700

    telerivet

--- src/main/java/com/sen4ik/vfb/controllers/TelerivetController.java
+++ src/main/java/com/sen4ik/vfb/controllers/TelerivetController.java
@@ -1,14 +1,21 @@
 package com.sen4ik.vfb.controllers;
 
-import com.sen4ik.vfb.entities.TelerivetIncoming;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 @Slf4j
@@ -34,13 +41,14 @@ public class TelerivetController {
             produces = MediaType.APPLICATION_JSON_VALUE
         )
 //    public @ResponseBody String telerivetHook(@PathVariable("secret") String secret, MultiValueMap paramMap){
-    public @ResponseBody String telerivetHook(
-            @RequestParam("secret") String secret
+    public @ResponseBody
+    ResponseEntity<JSONObject> telerivetHook(
+            @RequestParam("secret") String secret, HttpServletRequest request, HttpServletResponse response
 //            @RequestParam("incoming_message") String incoming_message,
 //            @RequestParam("event") String event,
 //            @RequestParam("content") String content,
 //            @RequestParam("from_number") String from_number
-    ){
+    ) throws IOException, ServletException {
         log.info("CALLED: telerivetHook()");
 
         log.info("secret: " + secret);
@@ -49,8 +57,52 @@ public class TelerivetController {
 //        log.info("from_number: " + from_number);
 
 //        String res = incoming_message + " | " + event + " | " + content + " | " + from_number;
+        PrintWriter out = response.getWriter();
+
+        if (!webHookSecret.equals(request.getParameter("secret")))
+        {
+            response.setStatus(403);
+            out.write("Invalid webhook secret");
+            log.info("Invalid webhook secret");
+        }
+        else if ("incoming_message".equals(request.getParameter("event")))
+        {
+            String content = request.getParameter("content");
+            String fromNumber = request.getParameter("from_number");
+            String phoneId = request.getParameter("phone_id");
+
+            // do something with the message, e.g. send an autoreply
+            response.setContentType("application/json");
+
+            try
+            {
+                JSONArray messages = new JSONArray();
+                JSONObject reply = new JSONObject();
+                reply.put("content", "Thanks for your message!");
+                messages.put(reply);
+
+                JSONObject json = new JSONObject();
+                json.put("messages", messages);
+
+                json.write(out);
+
+                return ResponseEntity.status(HttpStatus.OK)
+                        .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .body(json);
+            }
+            catch (JSONException ex){
+                throw new ServletException(ex);
+            }
+        }
+
         String res = secret;
-        return res;
+
+
+        JSONObject json = new JSONObject();
+        json.put("messages", "Error");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .body(json);
     }
 
 }
