e8ab35be13fb5b3f44fbd2e9dc58063bf83533a4
Artur Sentsov <sen4ik@gmail.com>
2020-04-13 21:49:43 -0700

    gitlab ci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+ORIGINAL_DB_NAME=$(awk '/spring.datasource.url/{print $NF}' "../${15}" | awk -F'/' '{print $4}' | awk -F'?' '{print $1}')
 GIT_COMMAND=""
 TEST_PREFIX=""
+UPDATED_DB_NAME=""
 if [[ "$1" == "vfb" ]]; then
     GIT_COMMAND="git fetch origin && git reset --hard origin/master"
+    # if its production db, leave db name as it is
+    UPDATED_DB_NAME=$ORIGINAL_DB_NAME
 elif [[ "$1" == "vfb_buggy" ]]; then
+    # reset buggy_version branch to head
     GIT_COMMAND="git fetch origin buggy_version && git reset --hard origin/buggy_version"
     TEST_PREFIX="dev:"
+    # we only need to update db name for the buggy version
+    UPDATED_DB_NAME=$1
 else
     echo "Unexpected parameter: ${1}"
     exit 1;
@@ -31,8 +38,10 @@ ssh -A sen4ik@$2 -p$3 "cd /home/sen4ik/workspace/${1} &&
       sed -i "/recaptcha.validation.secret-key=/c\recaptcha.validation.secret-key=${14}" ${15} &&
       sed -i "/recaptcha.testing.enabled=/c\recaptcha.testing.enabled=false" ${15} &&
       sed -i "/test.env.prefix=/c\test.env.prefix=${TEST_PREFIX}" ${15} &&
+      sed -i -e "s/$ORIGINAL_DB_NAME/$UPDATED_DB_NAME/g" ${15} &&
       mvn clean package -DskipTests=true &&
       cp target/vfb.war /opt/${1}/current/ &&
       sudo systemctl start ${1} &&
       sleep 10 &&
+      echo '' &&
       tail -n 15 /opt/${1}/logs/spring.log"
