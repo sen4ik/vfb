a6234272f4dc20e96360a5fcda131286eb216927
Artur Sentsov <sen4ik@gmail.com>
2020-09-28 20:56:10 -0700

    fixes

--- src/main/java/com/sen4ik/vfb/constants/Constants.java
+++ src/main/java/com/sen4ik/vfb/constants/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String WEBSITE_NAME = "www.VerseFromBible.com";
     public static final String SCHEMA = "verse_from_bible";
     public static final String reCaptchaFailedMessage = "reCaptcha failed on you! Are you a robot?";
-    public static final String generalMessage = "Hi, I am " + WEBSITE_NAME + " bot. If you want to subscribe - go to www.\" + WEBSITE_NAME + \". If you want to stop your subscription - reply STOP";
-    public static final String confirmSubscriptionMessage = "It looks like you have subscribed to \" + WEBSITE_NAME + \". If that is correct, reply YES.";
+    public static final String generalMessage = "Hi, I am " + WEBSITE_NAME + " bot. If you want to subscribe - go to www." + WEBSITE_NAME + ". If you want to stop your subscription - reply STOP";
+    public static final String confirmSubscriptionMessage = "It looks like you have subscribed to " + WEBSITE_NAME + ". If that is correct, reply YES.";
 
 }
